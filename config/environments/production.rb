e44906b1859a720e9c9bc1fafd9b9b9308f33740
ynjiun <ypw@att.net>
2013-03-12 11:03:51 -0700

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,4 +68,8 @@ Fojc::Application.configure do
   #need to make sure Heroku have same path for Paperclip?!
   Paperclip.options[:command_path] = "/usr/bin/"
     
+  # Force all access to the app over SSL, use Strict-Transport-Security, 
+  # and use secure cookies.
+  config.force_ssl = true
+    
 end
