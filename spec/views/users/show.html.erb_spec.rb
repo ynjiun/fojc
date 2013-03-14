be6bbd2bddb7d2bd5e59327add114a5d811524d0
ynjiun <ypw@att.net>
2013-03-14 16:05:35 -0700

    remove password_digest in show.html.erb

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -14,6 +14,6 @@ describe "users/show" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     rendered.should match(/Name/)
     rendered.should match(/Email/)
-    rendered.should match(/Password Digest/)
+    #rendered.should match(/Password Digest/)
   end
 end
