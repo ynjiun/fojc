5142de893c34490c42218ea61ca4e092631607ed
ynjiun <ypw@att.net>
2013-03-14 15:29:14 -0700

    Finish user edit, update, index and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
+  #before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :signed_in_user, :only => [:index, :edit, :update, :destroy]
+  before_filter :correct_user,   only: [:edit, :update]  
+  before_filter :admin_user,  only: :destroy
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page]) 
 
     respond_to do |format|
       format.html # index.html.erb
@@ -62,6 +66,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.update_attributes(params[:user])
+        flash[:success] = "Profile updated"
+        sign_in @user
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { head :no_content }
       else
@@ -76,10 +82,29 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-
+    flash[:success] = "User destroyed."
+      
     respond_to do |format|
       format.html { redirect_to users_url }
       format.json { head :no_content }
     end
-  end
+  end  
+  
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+  
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+  
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,8 +27,19 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) } 
+  it { should respond_to(:admin) }
     
   it {should be_valid}
+  it { should_not be_admin }
+    
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+    
+    it { should be_admin }
+  end
 
   describe "when name is not present" do
     before { @user.name = " "}
@@ -111,5 +122,31 @@ describe User do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
   end
+  
+#  describe "edit" do
+#    let(:user) { FactoryGirl.create(:user) }
+#    before do
+#      visit edit_user_path(user)
+#    end
+#
+#    describe "with valid information" do
+#      let(:new_name)  { "New Name" }
+#      let(:new_email) { "new@example.com" }
+#      before do
+#        fill_in "Name",             with: new_name
+#        fill_in "Email",            with: new_email
+#        fill_in "Password",         with: user.password
+#        fill_in "Confirm Password", with: user.password
+#        click_button "Save changes"
+#      end
+#
+#      it { should have_selector('title', text: new_name) }
+#      it { should have_selector('div.alert.alert-success') }
+#      it { should have_link('Sign out', href: signout_path) }
+#      specify { user.reload.name.should  == new_name }
+#      specify { user.reload.email.should == new_email }
+#    end
+#  
+#  end
   
 end
