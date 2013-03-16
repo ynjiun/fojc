22b7b3662063636820e4ff6e00c9e24e5db2b0a3
ynjiun <ypw@att.net>
2013-03-16 10:40:43 -0700

    delete unwanted files

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,7 +6,10 @@ describe "User pages" do
 
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
-    before { visit user_path(user) }
+    before do
+      sign_in user
+      visit user_path(user) 
+    end  
       
     #need webrat to use content: keyword!!!
     it { should have_selector('h1',    text: user.name) }
